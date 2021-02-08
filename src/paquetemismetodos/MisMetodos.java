2cd7b298befe1810b2b4ec5b19a937550cfe00e8
Damian Nogueiras <damiannogueiras@gmail.com>
2021-02-08 21:10:29 +0100

    modificacion para update

--- src/paquetemismetodos/MisMetodos.java
+++ src/paquetemismetodos/MisMetodos.java
@@ -3,5 +3,6 @@ package paquetemismetodos;
 public class MisMetodos {
     public static void hello(){
         System.out.println("Hello Lib diferente!");
+        System.out.println("Actualizado con git submodule update --remote LibreriaSubmodulo");
     }
 }
